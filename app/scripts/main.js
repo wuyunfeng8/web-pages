a751461d42a242139f080c2ecda1bbbeb2a24972
wuyunfeng <1363321437@qq.com>
2017-10-08 21:58:46 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,32 @@
 (function() {
-  'use strict';
-  // Your custom JavaScript goes here
-  
+    'use strict';
+    // Your custom JavaScript goes here
 
+    /**
+     * navbar
+     */
+
+    $('.ui.navbar .search-icon > i')
+        .click(function() {
+            let icon = $(this)
+            if (icon.text() === 'search') {
+                icon.text('close').parents('.content').addClass('searching')
+            } else {
+                icon.text('search').parents('.content').removeClass('searching')
+            }
+        })
+
+
+    /**
+     * sidebar
+     */
+
+    $('.ui.sidebar')
+        .sidebar('setting', 'dimPage', false)
+        .sidebar('attach events', '.ui.navbar .menu-icon')
+        .sidebar('attach events', '.ui.sidebar .close-icon')
+
+    $('.ui.navbar .menu')
+        .clone()
+        .appendTo('.ui.sidebar')
 })();
